4b7a33422b87f302ae15608400c3399dbdddf743
Christoph Paus <paus@mit.edu>
2014-01-28 13:37:22 -0500

    New synchrnonization of my youtube playlists.

--- bin/syncYouTubePls.pl
+++ bin/syncYouTubePls.pl
@@ -5,23 +5,19 @@ use URI;
 #===================================================================================================
 #
 # Script to automatically syncronize our local database with a list of playlists that we are
-# maintaining on youtube. This little script is based on a nice script ytplaylistfetcher which
-# is free software. The beauty of course is that a video that has alread been downloaded will
-# not be downloaded again. So just incremental changes will be applied, avoiding long download
-# times.
+# maintaining on youtube. This little script is based on an existing script ytplaylistfetcher
+# which is free software but really dod not do the job I wanted done. So I wrote my own and
+# rely now only on 'youtube-dl' a relatively well maintained tool to download youtube videos.
 #
-#  Packages to install:  ytplaylistfetcher (somewhere from the web)
+# The beauty of course is that a video that has already been downloaded will not be downloaded
+# again. So just incremental changes will be applied, avoiding long download times.
 #
-#                        - sudo yum install -y cclive clive lynx perl-Getopt-Long-Descriptive \
-#                                              perl-Web-Scraper  perl-LWP-Protocol-https \
-#                                              youtube-dl
-#
-#                        - mkdir $USER/Tools
-#                        - cd    $USER/Tools
-#                        - git clone git://git.carbon-project.org/ytplaylistfetcher.git
-#
+# Packages to install:  sudo yum install -y cclive clive lynx perl-Getopt-Long-Descriptive \
+#                                           perl-Web-Scraper  perl-LWP-Protocol-https \
+#                                           youtube-dl
 #
 #                                                                             Ch.Paus (Nov 06, 2011)
+#                                                                 Version 1.0 Ch.Paus (Jan 28, 2014)
 #===================================================================================================
 my $DATABASE="$ENV{'HOME'}/Music";
 my $PLAYLISTS="./PlayLists.txt";
@@ -30,6 +26,7 @@ if ("$ARGV[0]" != "") {
 }
 
 # Is the List of PlayList really there?
+
 if (-e "$PLAYLISTS") {
 	printf " List of playlists (exists): $PLAYLISTS\n";
 }
@@ -60,49 +57,66 @@ while (<INPUT>) {
 	my $dir  = $f[0];
 	my $plId = $f[1];
 	
-	printf "LINE - $_ -->  dir=$dir  plId=$plId\n";
-
+	printf "\n============================================================\n";
+	printf "  Next Playlist  -->  dir=$dir  plId=$plId\n";
+	printf   "============================================================\n\n";
 
 	my $oUri = URI->new('https://www.youtube.com/playlist?list='.$plId) ||
 			printf " ERROR -- Could not create URL object!  EXIT!\n";
-
-	my $oVideos = scraper {process '//a[contains(@class,"ux-thumb-wrap yt-uix-sessionlink")]',
-												 "videos[]" => '@href';
-	};
-
+	my $oVideos = scraper { process '//a[contains(@class,"ux-thumb-wrap yt-uix-sessionlink")]',
+													"videos[]" => '@href'; };
 	my $results = $oVideos->scrape($oUri);
 
-	foreach my $videoUrl (@{$results->{videos}}) {
+  # make list of existing files
+	my %existingTitlesHash = {};
+	opendir(DIR, $dir) or die $!;
+	my @files = grep {!/^\./} readdir(DIR);
 
-		my $videoId = $videoUrl;
-		$videoId =~ s/^(https?:\/\/www\.youtube\.com\/watch\?)(.*)?(v=[a-zA-Z0-9_\-]+)(.*)$/$3/;
-		$videoId = substr($videoId,2);
-		
-		printf "URL: $videoUrl -> $videoId\n";
-
-		my $DL_CMD = "youtube-dl -o '%(title)s.%(ext)s' https://www.youtube.com/watch?v=$videoId";
-		printf " DOWLOAD: $DL_CMD\n";
+	for my $file (@files) {
+		print " Existing -- $file\n";
+		my $hash = `cleanString.sh cleanContractBasename \"$file\"`; chop($hash);
+				if (exists $existingTitlesHash{$hash}) {
+			printf "\n Requesting already filled spot: $hash -> $file (exists: $existingTitlesHash{$hash})\n";
+			my $cmd = " rm \"$dir/$file\"";
+			print(" Execute: $cmd\n\n");
+			system($cmd);
+		}
+		else {
+			$existingTitlesHash{$hash} = "$file";
+			printf " Added hash: $hash -> $file\n";
+		}
 	}
+	closedir(DIR);
 
-#
-#  dir=`   echo $line | tr -s ' ' | cut -d ' ' -f1`
-#  pLstId=`echo $line | tr -s ' ' | cut -d ' ' -f2`
-#
-#
-#  # The meat: do the synchronization
-#
-#  echo ""
-#  echo " Synchronizing playlist: $dir (id: $pLstId) "
-#  mkdir -p $DATABASE/$dir
-#  cd       $DATABASE/$dir
-#  echo " ytplaylistfetcher -d http://www.youtube.com/playlist?list=$pLstId >& sync.log"
-#  echo " Execute:  ytplaylistfetcher -d  http://www.youtube.com/playlist?list=$pLstId"  > sync.log
-#  echo " "                                                                             >> sync.log
-#  #ytplaylistfetcher -d http://www.youtube.com/playlist?list=$pLstId                    >> sync.log
-#
-#
-#  
-#done < $PLAYLISTS
+	# process the URLs in this list and download if needed
+	chdir($dir);
+ 	foreach my $videoUrl (@{$results->{videos}}) {
+
+ 		my $videoId = $videoUrl;
+
+ 		$videoId =~ s/^(https?:\/\/www\.youtube\.com\/watch\?)(.*)?(v=[a-zA-Z0-9_\-]+)(.*)$/$3/;
+ 		$videoId = substr($videoId,2);
+ 		
+ 		printf "\nURL: $videoUrl -> $videoId\n";
+
+		# get file name
+ 		my $dl_cmd = "youtube-dl -o '%(title)s.%(ext)s' https://www.youtube.com/watch?v=$videoId";
+ 		my $file = `$dl_cmd --get-filename`;  chop($file);
+ 		my $hash = `cleanString.sh cleanContractBasename \"$file\"`; chop($hash);
+
+		# test whether file or something very similar exists already locally
+ 		printf " Testing hash: $hash  -->  file: $file\n";
+
+ 		if (exists $existingTitlesHash{$hash}) {
+ 			printf " Matching file already found: $hash -> $file (exists: $existingTitlesHash{$hash})\n";
+ 		}
+		else {
+			print(" Execute: $dl_cmd\n\n");
+			system($dl_cmd);
+		}
+ 		
+ 	}
+	chdir("../");
 
 }
 
