6cad95a0eaa7378dccde48ab92bcb564bc668f1b
Christoph Paus <paus@mit.edu>
2014-03-29 19:43:00 -0400

    Configuration for github remote.

---
+++ githubRemote.sh
@@ -0,0 +1,8 @@
+ git remote set-url origin git@github.com:cpausmit/MitCommon
+ git remote set-url origin git@github.com:cpausmit/MitAna
+ git remote set-url origin git@github.com:cpausmit/MitProd
+ git remote set-url origin git@github.com:cpausmit/MitEdm
+ git remote set-url origin git@github.com:cpausmit/MitPlots
+ git remote set-url origin git@github.com:cpausmit/Config
+ git remote set-url origin git@github.com:cpausmit/IntelROCCS
+ git remote set-url origin git@github.com:cpausmit/DynamicData
