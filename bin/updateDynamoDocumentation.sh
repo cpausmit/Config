4ffd87f43414627c83c08c6d9fa1408d0c27cd5e
Christoph Paus <paus@mit.edu>
2018-06-13 23:47:17 -0400

    INtroduce option to only build locally.

--- bin/updateDynamoDocumentation.sh
+++ bin/updateDynamoDocumentation.sh
@@ -3,6 +3,8 @@
 # Little script to completely update the dynamo documentation.
 #
 
+LOCAL=$1
+
 pwd=`pwd`
 basedir=`basename $pwd`
 if [ "$basedir" != 'dynamo-docs' ]
@@ -11,20 +13,22 @@ then
     exit 1
 fi
 
-git pull
-echo -n "continue ? [N/y]"
-read YES
-if [ "$YES" != 'y' ]
+
+if [ "$LOCAL" == "" ]
 then
-    echo "EXIT - fix the pull"
-    exit 0
+    git pull
+    echo -n "continue ? [N/y]"
+    read YES
+    if [ "$YES" != 'y' ]
+    then
+	echo "EXIT - fix the pull"
+	exit 0
+    fi
+    git commit -am 'Working on v0.0'
+    git push -u origin master
 fi
 
-git commit -am 'Working on v0.0'
-git push -u origin master
-
 sphinx-build -a -E . ~/dynamo-documentation
-
 rsync -Cavz --delete ~/dynamo-documentation t3home000.mit.edu:public_html
 
 exit 0
