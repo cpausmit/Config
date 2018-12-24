42e670db85419b281cce3ee75c9c73d9579eb29e
Christoph Paus <paus@mit.edu>
2018-12-24 15:53:53 -0500

    Installation updates.

--- bin/mountExport.sh
+++ bin/mountExport.sh
@@ -2,28 +2,34 @@
 #===================================================================================
 # Try to mount the export drive.
 #===================================================================================
+MOUNT_POINT="/export"
 
-echo ""
-echo " Test whether /export mounted already"
-echo ""
-if [ ".`df -h |grep /export`" != "." ]
+# check whether mount point exists and create if needed
+if [ -d "$MOUNT_POINT" ]
+then
+  echo " Mount point ($MOUNT_POINT) exists."
+else
+  echo " Mount point ($MOUNT_POINT) does not exists. Creating it now."
+  sudo mkdir -p "$MOUNT_POINT"
+fi
+
+# Test whether $MOUNT_POINT mounted already"
+if [ ".`df -h |grep $MOUNT_POINT`" != "." ]
 then
   echo " -> already mounted, exit here!"
-echo ""
+  echo ""
   exit 0
 fi
 
-echo ""
-echo " Trying to mount external drive -- start loop"
-echo ""
-for drive in b c d
+# Trying to mount external drive -- start loop
+for drive in `sudo fdisk -l| grep ^/dev/sd|cut -c8|sort -u`
 do
   test=`sudo fdisk -l /dev/sd${drive} |tr -s ' '|grep dev/sd${drive}1 2> /dev/null`
   echo " -> tested drive: $drive -- result: $test"
   if [ ".$test" == "./dev/sd${drive}1 63 3907029167 3907029105 1.8T 83 Linux" ]
   then
     echo "Try to mount /dev/sd${drive}"
-    sudo mount /dev/sd${drive}1 /export
+    sudo mount /dev/sd${drive}1 $MOUNT_POINT
     echo ""
     exit 0
   fi
