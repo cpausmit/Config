d0749f1b3918e50d6bbf12ef137a52d788b05a22
Christoph Paus <paus@mit.edu>
2022-09-27 15:49:24 -0400

    Install Fedora 36.

---
+++ install/init.sh
@@ -0,0 +1,10 @@
+##
+## As root:  echo "paus ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+##
+
+## Add group/user
+#sudo groupadd -g 1399 zh
+#sudo useradd  -s /bin/bash -d /home/paus -u 5407 -g zh -c 'Christoph Paus' paus
+#sudo dnf install -y git
+#cd; git clone http://github.com/cpausmit/Config
+#cd Config; ./Configure.sh
