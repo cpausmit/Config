6e8a8aedc4a69a535e07b4e4dbf73f1081d75ba8
Christoph Paus <paus@mit.edu>
2024-10-25 22:04:54 -0400

    Update mit-group-incremental.sh

--- backup/mit-group-incremental.sh
+++ backup/mit-group-incremental.sh
@@ -8,12 +8,13 @@ echo ""
 # -mindepth 1: Don't apply the tests to $BU_TARGET itself (depth 0).
 # -maxdepth 1: Don't go deeper than what's just below $BU_TARGET (depth 1).
 # -type d: We're only interested in directories.
-# -not -nouser: Must have owner UIDs defined in the passwd file (or NIS map).
+### -not -nouser: Must have owner UIDs defined in the passwd file (or NIS map).
 # -not -name lost+found: Useless to back this up.
 
 export EXCLUDE_USERS=`cat $BU_TARGET/.exclude-users | tr '\n' ' ' 2> /dev/null`
 
-for LUZER in `find $BU_TARGET -mindepth 1 -maxdepth 1 -type d -and -not -nouser -and -not -name lost+found -printf "%f "`
+##for LUZER in `find $BU_TARGET -mindepth 1 -maxdepth 1 -type d -and -not -nouser -and -not -name lost+found -printf "%f "`
+for LUZER in `find $BU_TARGET -mindepth 1 -maxdepth 1 -type d -and -not -name lost+found -printf "%f "`
 do
 
   if [ ".`echo " $EXCLUDE_USERS " | grep " $LUZER "`" != "." ]
